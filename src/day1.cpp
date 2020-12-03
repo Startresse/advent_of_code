f40930600bf9a0d45038b4f4e12bb1ea2cd5ed19
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-03 09:48:41 +0100

    day1 done

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,5 +1,29 @@
 #include "day1.h"
 
 void Day1::run() {
-    std::cout << "test" << std::endl;
+    std::ifstream file;
+    file.open("inputs/input_d1.txt");
+
+    std::vector<int> values;
+    int value;
+    while(file >> value)
+        values.push_back(value);
+
+    for (uint i = 0; i < values.size(); ++i) {
+        for (uint j = 0; j < values.size(); ++j) {
+            for (uint k = 0; k < values.size(); ++k) {
+                if (i == j || j == k || i == k)
+                    continue;
+
+                if (values[i] + values[j] + values[k] == 2020) {
+                    std::cout << values[i] * values[j] * values[k] << std::endl;
+                    goto quit_loop;
+                }
+
+            }
+        }
+    }
+    quit_loop:
+
+    file.close();
 }
--- src/day1.h
+++ src/day1.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 class Day1 {
     private:
