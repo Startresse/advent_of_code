c1a795ed0ca0f6a35adafb8e26810e2675541b93
p1516523 <tristan.guillard@etu.univ-lyon1.fr>
2020-12-04 10:26:45 +0100

    day4 complete

--- src/day4.cpp
+++ src/day4.cpp
@@ -1,5 +1,95 @@
 #include "day4.h"
 
+bool Passport::is_north_pole() {
+    return
+        byr != -1 &&
+        iyr != -1 &&
+        eyr != -1 &&
+        hgt != "" &&
+        hcl != "" &&
+        ecl != "" &&
+        pid != "";
+}
+
+bool Passport::is_complete() {
+    return is_north_pole() && cid != -1;
+}
+
+void Passport::add_field(std::string s) {
+    std::string field = s.substr(0, 3);
+
+    size_t end = s.size() - 1;
+    if (field == "byr")
+        byr = atoi(s.substr(4, end).c_str());
+    else if (field == "iyr")
+        iyr = atoi(s.substr(4, end).c_str());
+    else if (field == "eyr")
+        eyr = atoi(s.substr(4, end).c_str());
+    else if (field == "hgt")
+        hgt = s.substr(4, end);
+    else if (field == "hcl")
+        hcl = s.substr(4, end);
+    else if (field == "ecl")
+        ecl = s.substr(4, end);
+    else if (field == "pid")
+        pid = s.substr(4, end);
+    else if (field == "cid")
+        cid = atoi(s.substr(4, end).c_str());
+    else {
+        std::cout << field << " : ";
+        std::cout << "invalid type" << std::endl;
+
+    }
+
+
+}
+
+const std::vector<std::string> ecl_types = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+const std::regex hcl_check("#[a-f0-9]{6}");
+const std::regex pid_check("[0-9]{9}");
+bool Passport::is_valid() {
+    if (!is_north_pole())
+        return false;
+
+    if (byr < 1920 || byr > 2002)
+        return false;
+
+    if (iyr < 2010 || iyr > 2020)
+        return false;
+    
+    if (eyr < 2020 || eyr > 2030)
+        return false;
+
+    size_t sz = hgt.size();
+    std::string hgt_tp = hgt.substr(sz - 2, sz - 1);
+    int hgt_i = atoi(hgt.substr(0, sz - 2).c_str());
+    if (hgt_tp == "cm") {
+        if (hgt_i < 150 || hgt_i > 193)
+            return false;
+    } else if (hgt_tp == "in") {
+        if (hgt_i < 59 || hgt_i > 76)
+            return false;
+    } 
+    else
+        return false;
+
+    if(!std::regex_match(hcl, hcl_check))
+        return false;
+    
+    bool found = false;
+    for (auto s : ecl_types) {
+        if (ecl == s)
+            found = true;
+    }
+    if (!found)
+        return false;
+
+    if(!std::regex_match(pid, pid_check))
+        return false;
+
+    return true;
+}
+
 void Day4::run() {
 
     std::ifstream file;
@@ -9,7 +99,27 @@ void Day4::run() {
         return;
     }
 
-    std::cout << "Hello world" << std::endl;
-    file.close();
+    int count = 0;
+    std::string line;
+    while (getline(file, line)) {
+        Passport p;
+        
+        while(line != "") {
+            std::stringstream ss(line);
 
+            std::string field;
+            while(ss >> field) {
+                p.add_field(field);
+            }
+
+            getline(file, line);
+        }
+
+        if (p.is_valid())
+            count++;
+    }
+
+    std::cout << count << std::endl;
+
+    file.close();
 }
--- src/day4.h
+++ src/day4.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <regex>
 
 class Day4 {
     private:
@@ -14,3 +15,35 @@ class Day4 {
 
         void run();
 };
+
+class Passport {
+public:
+    int byr = -1;
+    int iyr = -1;
+    int eyr = -1;
+    std::string hgt = "";
+    std::string hcl = "";
+    std::string ecl = "";
+    std::string pid = "";
+    int cid = -1;
+
+    bool is_north_pole();
+    bool is_complete();
+
+    bool is_valid();
+
+    void add_field(std::string);
+    friend std::ostream& operator<<(std::ostream& os, const Passport& p);
+};
+
+std::ostream& operator<<(std::ostream& os, const Passport& p) {
+    os << "byr:" << p.byr << std::endl;
+    os << "iyr:" << p.iyr << std::endl;
+    os << "eyr:" << p.eyr << std::endl;
+    os << "hgt:" << p.hgt << std::endl;
+    os << "hcl:" << p.hcl << std::endl;
+    os << "ecl:" << p.ecl << std::endl;
+    os << "pid:" << p.pid << std::endl;
+    os << "cid:" << p.cid << std::endl;
+    return os;
+}
